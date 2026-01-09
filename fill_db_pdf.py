80f922fad9abb24c2839ee2f74d2970a0450259f
lisa-wen <lisa.wenige@hs-merseburg.de>
2026-01-09 15:57:13 +0100

    update fill_db_pdf.py

--- fill_db_pdf.py
+++ fill_db_pdf.py
@@ -7,73 +7,93 @@ from dotenv import load_dotenv
 
 load_dotenv()
 documents = {
-    "doc1" : {
-        "title" : "Einführung in die Verfahrenstechnik",
-        "path" : "sources/ZOGG.pdf"
-    },
-    "doc2" : {
-        "title" : "Krümelkunde",
-        "path" : "sources/Kruemelkunde_2016.pdf"
-    }
-}
-# PDF laden
+     "doc1" : {
+         "title" : "Einführung in die Verfahrenstechnik",
+         "path" : "sources/ZOGG.pdf"
+     },
+     "doc2" : {
+         "title" : "Krümelkunde",
+         "path" : "sources/Kruemelkunde_2016.pdf"
+     }
+ }
+# # PDF laden
 pages = []
 for key, doc in documents.items():
-    reader = PdfReader(doc["path"])
-    for page_number, page in enumerate(reader.pages, start=1):
-        text = page.extract_text()
-        if text:
-            pages.append({
-                "page": page_number,
-                "text": text,
-                "title": doc["title"]
-            })
+     reader = PdfReader(doc["path"])
+     for page_number, page in enumerate(reader.pages, start=1):
+         text = page.extract_text()
+         if text:
+             pages.append({
+                 "page": page_number,
+                 "text": text,
+                 "title": doc["title"]
+             })
 
 print(f"Loaded PDF with {len(pages)} pages")
 
 # Text in kleinere Bestandteile (Chunks) aufteilen
 text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=800,
-    chunk_overlap=100,
-)
-
-# Vektordatenbank einrichten und Kollektion erstellen
-client = chromadb.PersistentClient(path="./chroma_db")
+     chunk_size=800,
+     chunk_overlap=100,
+ )
+#
+# # Vektordatenbank einrichten und Kollektion erstellen
+client = chromadb.PersistentClient(path="chroma_db")
 emb = embedding_functions.SentenceTransformerEmbeddingFunction(
-    model_name="jinaai/jina-embeddings-v2-base-de"
+     model_name="jinaai/jina-embeddings-v2-base-de",
+     device="cpu"
 )
 collection = client.get_or_create_collection(
-    "verfahrenstechnik",
-    embedding_function=emb
-)
-
+     "verfahrenstechnik",
+     embedding_function=emb
+ )
+#
 ids = []
 metadatas = []
 chunks = []
 for page in pages:
-    page_chunks = text_splitter.split_text(page["text"])
+     page_chunks = text_splitter.split_text(page["text"])
 
-    for chunk in page_chunks:
-        chunks.append(chunk)
-        metadatas.append({
-            "source": page["title"],
-            "page": page["page"]
-        })
-        ids.append(str(uuid4()))
+     for chunk in page_chunks:
+         chunks.append(chunk)
+         metadatas.append({
+             "source": page["title"],
+             "page": page["page"]
+         })
+         ids.append(str(uuid4()))
 
 collection.add(
-    documents=chunks,
-    metadatas=metadatas,
-    ids=ids
+     documents=chunks,
+     metadatas=metadatas,
+     ids=ids
 )
 
 print("Stored PDF in Chroma.")
 
 # Die Datenbank abfragen
 query = "Welche anderen Filtrationsmethoden gibt es außer Druckfiltration?"
-results = collection.query(
+results1 = collection.query(
+    query_texts=[query],
+    n_results=5,
+)
+results2 = collection.query(
     query_texts=[query],
     n_results=5,
 )
 
-print(results)
+results1 = collection.query(
+    query_texts=[query],
+    n_results=5,
+)
+results3 = collection.query(
+    query_texts=[query],
+    n_results=5,
+)
+
+
+print(results1)
+print(results2)
+print(results3)
+print(collection.count())
+print(collection.count())
+print(collection.count())
